01ca368bdbe8949542babfe82adaad7c281621d2
iamvsc04 <saicharanstar22@gmail.com>
2025-05-17 03:41:42 +0530

    done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-  faEnvelope, 
-  faMapMarkerAlt, 
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMapMarkerAlt,
   faCode,
   faLaptopCode,
-  faTrophy
-} from '@fortawesome/free-solid-svg-icons';
-import { 
-  faGithub, 
+  faTrophy,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
   faLinkedin,
-  faInstagram
-} from '@fortawesome/free-brands-svg-icons';
-import '../styles/Contact.scss';
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import "../styles/Contact.scss";
 
 const Contact = () => {
   return (
@@ -26,71 +26,71 @@ const Contact = () => {
           <div className="contact-info">
             <div className="profiles-grid">
               {/* Coding Profiles */}
-              <a 
-                href="https://leetcode.com/Sai_Charan04" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://leetcode.com/Sai_Charan04"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="LeetCode"
               >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a 
-                href="https://www.codechef.com/users/iamvsc04" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://www.codechef.com/users/iamvsc04"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="CodeChef"
               >
                 <FontAwesomeIcon icon={faLaptopCode} />
               </a>
-              <a 
-                href="https://www.hackerrank.com/veerlapatisaich1" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://www.hackerrank.com/iamvsc04"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="HackerRank"
               >
                 <FontAwesomeIcon icon={faTrophy} />
               </a>
-              <a 
-                href="https://github.com/iamvsc04" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://github.com/iamvsc04"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="GitHub"
               >
                 <FontAwesomeIcon icon={faGithub} />
               </a>
-              
+
               {/* Personal Profiles */}
-              <a 
-                href="mailto:veerlapatisaicharan04@gmail.com" 
+              <a
+                href="mailto:veerlapatisaicharan04@gmail.com"
                 className="profile-icon"
                 title="Email Me"
               >
                 <FontAwesomeIcon icon={faEnvelope} />
               </a>
-              <a 
-                href="https://linkedin.com/in/sai-charan-veerlapati" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://linkedin.com/in/sai-charan-veerlapati"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="LinkedIn"
               >
                 <FontAwesomeIcon icon={faLinkedin} />
               </a>
-              <a 
-                href="https://instagram.com/your_instagram" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://instagram.com/iamvsc4"
+                target="_blank"
+                rel="noreferrer"
                 className="profile-icon"
                 title="Instagram"
               >
                 <FontAwesomeIcon icon={faInstagram} />
               </a>
-              <a 
-                href="#" 
+              <a
+                href="#"
                 className="profile-icon location"
                 title="Hyderabad, India"
               >
@@ -104,4 +104,4 @@ const Contact = () => {
   );
 };
 
-export default Contact; 
+export default Contact;
